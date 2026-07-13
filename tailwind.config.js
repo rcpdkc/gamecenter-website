2829aa24da62d29a1b423262f6a5089899dc2104
Game Center <gamecenter@local>
2026-07-14 00:30:20 +0300

    Fix CSS: Install and configure Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "bg-primary": "#0a0b10",
+        "text-main": "#f1f5f9"
+      }
+    },
+  },
+  plugins: [],
+}
+
