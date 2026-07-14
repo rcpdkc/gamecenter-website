fe29080ae8ec47517ab5af637a0fdaaaad2c93a0
Game Center <gamecenter@local>
2026-07-14 23:20:45 +0300

    Fix CORS policy error in mklink_archive api

--- api/mklink_archive.js
+++ api/mklink_archive.js
@@ -1,6 +1,19 @@
 import { sql } from '@vercel/postgres';
 
 export default async function handler(request, response) {
+  response.setHeader('Access-Control-Allow-Credentials', true);
+  response.setHeader('Access-Control-Allow-Origin', '*');
+  response.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+  response.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  );
+
+  if (request.method === 'OPTIONS') {
+    response.status(200).end();
+    return;
+  }
+
   try {
     // Ensure table exists
     await sql`
